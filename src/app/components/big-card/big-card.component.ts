da45215bc90a2df26bbc3ef7de526113054bd841
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-04-27 13:33:44 -0300

    Creates dynamic elements

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit {
+
+  @Input() bigCardTitle: string ='';
+  @Input() bigCardPicture: string = '';
+  @Input() bigCardDescription: string = '';
 
   constructor() { }
 
