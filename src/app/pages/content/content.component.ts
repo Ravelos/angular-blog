b2f3aa3283bb5b7cc8ba31f279a97d0917ccd80c
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-04-27 23:11:39 -0300

    Creates the components properties

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
 
+  photoCover: string = '';
+  contentTitle: string = '';
+  contentDescription: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
