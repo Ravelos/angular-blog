3dc860f4086bedb4d5653a90b6a02e16a1f90014
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-04-30 15:45:46 -0300

    Sets the values according to the article

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { fakeData } from 'src/app/data/fakeData';
 
 @Component({
   selector: 'app-content',
@@ -10,15 +12,25 @@ export class ContentComponent implements OnInit {
   photoCover: string = '';
   contentTitle: string = '';
   contentDescription: string = '';
+  private id:string | null = "0";
 
   constructor(
     private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(values =>
-      console.log(value.get("id"))
+    this.route.paramMap.subscribe(value =>
+      this.id = value.get("id")
     )
+    this.setValuestoComponent(this.id);
+  }
+
+  setValuestoComponent(id:string | null){
+    const result = fakeData.filter(
+       article => article.id === id)[0]
+      this.contentTitle = result.title;
+      this.contentDescription = result.description;
+      this.photoCover = result.photoCover;
   }
 
 }
