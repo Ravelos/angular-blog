da7b3ab304d59fd7076c0bdb864cc9e6b241a024
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-04-30 15:41:29 -0300

    Updates the data to make it more dynamic

--- src/app/data/fakeData.ts
+++ src/app/data/fakeData.ts
@@ -1,8 +1,26 @@
 export const fakeData = [
   {
   id:'1',
-  photo:'',
-  title:'',
-  description: ''
+  title:'New Iron Man movie coming soon',
+  description: 'Robert Downey Jr. may return to the role of Tony Stark/Iron Man in the upcoming movie Doctor Strange in the Multiverse of Madness. According to bgr.com, a leak has suggested that Tom Cruise will play a Tony Stark from an alternate timeline, who is part of the Illuminati and possesses three of the Infinity Stones.',
+  photoCover:'https://media.contentapi.ea.com/content/dam/news/www-ea/common/ea-motive-new-title-teaser-16x9.jpg.adapt.1456w.jpg',
+  },
+  {
+    id:'2',
+    title:'Guardian of The Galaxy Vol.3',
+    description: "Guardians of the Galaxy Vol 3 has been a long time coming for various reasons, and now it feels like it's arriving at a pivotal time for Marvel Studios.",
+    photoCover:'https://cdn.marvel.com/content/1x_masonry/gotg_vol3_1080x1920_reald_digitalposter_v5.jpg',
+  },
+  {
+    id:'3',
+    title:'The Invincible Iron Man',
+    description: "A new Iron Age begins! From the violent streets of Chicago, a new armored hero rises! Clad in her very own Iron Man armor, Riri Williams is ready to show the world what she can do as the self-made hero of tomorrow. Her technology just might change the face of the Marvel Universe forever...if she survives the experience. But is she ready for all the problems that come with stepping into Iron Man's jet boots? Where's a genius billionaire playboy philanthropist when you need one?",
+    photoCover:'https://cdn.marvel.com/u/prod/marvel/i/mg/3/e0/63b476756e35b/clean.jpg',
+  },
+  {
+    id:'4',
+    title:'The Amazing Spiderman and Nightcrawler',
+    description: "When reading about an old friend from his days in the circus who had been gunned down by a mysterious sniper, the X-Men's Nightcrawler decides to go to Coney Island to investigate. As fate would have it, Peter Parker would also be spending the day at Coney Island on a date with Mary Jane, when the sniper strikes again. This prompts Peter to excuse himself from his date and change into Spider-Man before trying to catch the sniper. He ends up running into Nightcrawler, whom he assumes is the killer, when he catches the mutant with the sniper's weapon. Their fight takes them to the amusement park, but Nightcrawler gets away with his teleportation powers and Spider-Man is forced to flee when the police arrive.",
+    photoCover:'https://cdn.marvel.com/u/prod/marvel/i/mg/3/e0/63b476756e35b/clean.jpg',
   }
 ]
